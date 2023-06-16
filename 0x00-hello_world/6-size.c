e0d1bf4cf5712e3682a6dcbcb348e1c31db03fcc
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-16 15:32:58 -0700

    Printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Program to diplay a string
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+int b;
+long c;
+long long int d;
+float f;
+printf("Size of char: '%c' byte(s), );
+}
+return (0);
