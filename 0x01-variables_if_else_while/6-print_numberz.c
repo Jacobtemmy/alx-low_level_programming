5e36834acfb91af2a65a9cbe7264948b1cbfa5cd
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-18 07:44:29 -0700

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
