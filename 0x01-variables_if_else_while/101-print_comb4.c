278a5e0b224d5d9ffe05ffe160eb63313b306ed5
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-18 11:44:41 -0700

    Combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ while (a < 10)
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');
-				if (a != 7 || b != 8 && c != 9)
+				if (a != 7 || b != 8 || c != 9)
 				{
 					putchar(',');
 					putchar(' ');
