504f7fe4681cfe8b6c718d93bf907b3d69fc50d4
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-20 06:37:55 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Function - A function to print alphabets
+ * Return: 0 Success
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+_putchar('\n');
+}
