458a1730cbbf97219bdc32c2ef90cab8b99eca75
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-21 06:11:29 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - A function a print the absolute of a number
+ * @num: to accept integer
+ * Return:to return  num
+ */
+int _abs(int num)
+{
+	num = num;
+
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
