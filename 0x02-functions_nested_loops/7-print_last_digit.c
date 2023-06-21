91757eeaed7899524511347cc1617b539dbdae1a
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-21 06:37:43 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,12 @@
  */
 int print_last_digit(int r)
 {
-	r = r % 10;
+int lastdigit = r % 10;
 
-	{
-		_putchar('0' + r);
-	}
-return (r);
+if (lastdigit < 0)
+{
+lastdigit  = -lastdigit;
+}
+_putchar('0' + lastdigit);
+return (lastdigit);
 }
