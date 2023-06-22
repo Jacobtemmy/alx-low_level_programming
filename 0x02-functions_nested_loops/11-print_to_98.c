73576888fe2331d9df5b48f54b1e28b3c68983a5
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-22 01:43:58 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,23 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
-	{
-		printf("%d", n);
-		if (n != 98)
-		{
-			printf(", ");
-		}
-		n++;
-	}
-	while (n > 98)
-	{
-		printf("%d", n);
-		if (n != 98)
-			{
-				printf(", ");
-			}
-		n--;
-	}
+while (n < 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
+}
+while (n > 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
 printf("98\n");
 }
