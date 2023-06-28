47393dd001f177691c90144581dcb733366b425a
Jacobtemmy <jacobtemmy02@gmail.com>
2023-06-28 14:38:52 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,26 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (j < 10 && result < 10)
+			if (result < 10)
 			{
 				_putchar('0' + result);
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar('0' + result / 10);
 				_putchar('0' + result % 10);
 			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+	_putchar('\n');
 	}
 }
