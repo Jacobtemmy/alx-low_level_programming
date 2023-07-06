89a44bc71e5059d138692131ec697acea776dcd3
Jacobtemmy <jacobtemmy02@gmail.com>
2023-07-05 20:06:57 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - A raise to power function
+ * @x: The base number
+ * @y: The power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
