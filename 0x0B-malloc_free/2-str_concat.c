524108a1b69dcac63c6323884bd106c841c5f4d2
Jacobtemmy <jacobtemmy02@gmail.com>
2023-07-11 19:48:41 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - A function that returns a pointer to a newly allocated
+ * @s1: A string
+ * @s2: A string
+ * Return: A character
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	char *p = (char *) malloc(len1 + len2 + 1);
+
+	if (p == 0)
+	{
+		return (0);
+	}
+	strcpy(p, s1);
+	strcat(p, s2);
+	return (p);
+}
