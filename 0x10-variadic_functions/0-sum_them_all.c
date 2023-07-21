38970029d092641abb791acb5cc9534bfe49b2f4
Jacobtemmy <jacobtemmy02@gmail.com>
2023-07-21 06:05:45 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,9 @@
-#include "variadic_functions.h" 
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 /**
  * sum_them_all - A function to sum all integers
- * @n - An integer valie
+ * @n: An integer value
  * Return: An integer
  */
 int sum_them_all(const unsigned int n, ...)
@@ -11,6 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int result = 0;
 	va_list summing;
+
 	va_start(summing, n);
 	if (n == 0)
 	{
@@ -20,9 +21,9 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			result = result + va_arg (summing, int); 
+			result = result + va_arg(summing, int);
 		}
 	}
-	va_end (summing);
+	va_end(summing);
 	return (result);
 }
