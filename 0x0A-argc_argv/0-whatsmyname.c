3abe506a826d45a9680ad63343b8b80c4e043a57
Jacobtemmy <jacobtemmy02@gmail.com>
2023-10-02 05:49:45 -0700

    It aint what they call you, its what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 /**
  * main - A program to print the name of the file
+ * @argc: argument count
+ * @argv: argument
  * Return: 0 Success
  */
 int main(int argc, char *argv[])
